87e1f67bc70ed22e1c62235043b5bdf50556b6d9
ChocoPic <60088300+ChocoPic@users.noreply.github.com>
2022-10-22 18:04:01 +0900

    검색화면-키워드선택기능추가

--- demo/src/main/java/com/example/demo/repository/CafeRepository.java
+++ demo/src/main/java/com/example/demo/repository/CafeRepository.java
@@ -3,7 +3,4 @@ package com.example.demo.repository;
 import com.example.demo.db.Cafe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CafeRepository extends JpaRepository<Cafe,Integer> {
-//    @Query("SELECT u FROM UserEntity u WHERE u.name = :name")
-//    List<UserEntity> findByName(String name);
-}
+public interface CafeRepository extends JpaRepository<Cafe,Integer> {}
